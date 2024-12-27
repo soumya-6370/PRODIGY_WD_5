d6c6ebeb308777cd1fb7a9b979e6a618953160ff
soumya-6370 <srsethy66@gmail.com>
2024-12-27 20:03:26 +0530

    Add files via upload

---
+++ weatherapp/script.js
@@ -0,0 +1,30 @@
+const APIKEY = 'c4b469446da64a62a7455412241903';
+
+const searchBtn = document.getElementById('searchBtn');
+const cityInput = document.getElementById('cityInput');
+
+// Referencing output fields
+const cityName = document.getElementById('city-name');
+const countryName = document.getElementById('countryName');
+const localTime = document.getElementById('loc-time');
+const temp = document.getElementById('temp');
+const sup = document.getElementById('sup');
+
+async function getData(KEY, cityName) {
+    const promise = await fetch(`https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${cityName}&aqi=yes`);
+    return await promise.json();
+}
+
+searchBtn.addEventListener('click', async () => {
+    const input = cityInput.value;
+    document.getElementById('outputCard').style.visibility = 'visible';
+
+    const result = await getData(APIKEY, input);
+  
+    // Fixing the use of template literals
+    cityName.innerText = `${result.location.name}, ${result.location.region}`;
+    countryName.innerText = `${result.location.country}`;
+    temp.innerText = `${result.current.temp_c}`;
+    sup.innerText = '°C';
+    localTime.innerText = `${result.location.localtime}`;
+});
